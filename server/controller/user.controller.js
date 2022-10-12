9749ebdc1bcf8a8a215a4a8664b11dc75f93458c
hikdul <hikdul.lio@gmail.com>
2022-10-12 17:21:42 -0500

    async state

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,16 +1,16 @@
 
-const CreateUser =  (req, res)=>{
-    res.json({ok:true})
+const CreateUser = async (req, res) => {
+    res.json({ ok: true })
 }
 
-const LoginUser =  (req, res)=>{
-    res.json({ok:true})
+const LoginUser = async (req, res) => {
+    res.json({ ok: true })
 }
 
-const RenewToken =  (req, res)=>{
-    res.json({ok:true})
+const RenewToken = async (req, res) => {
+    res.json({ ok: true })
 }
 
-module.exports ={
+module.exports = {
     CreateUser, LoginUser, RenewToken
 }
